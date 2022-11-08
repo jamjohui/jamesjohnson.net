80b8d7d6651aeac7c50ab2ed14597a0985360ced
James Johnson <jamjoh.ui@gmail.com>
2022-11-08 09:49:18 -0500

    Refactor Icon

--- src/components/Icon/Svg/Svg.tsx
+++ src/components/Icon/Svg/Svg.tsx
@@ -1,15 +1,9 @@
 export interface SvgProps {
-  /** CSS styling class */
   className?: string;
-
-  /** The SVG image data */
   path: string;
-
-  /** Size of the SVG */
   viewbox: string;
 }
 
-/** Renders an SVG image */
 export const Svg = ({ className, path, viewbox }: SvgProps): JSX.Element => (
   <svg
     className={className}
