54a56c399a555d77941142a261307d9613c77eb7
James Johnson <jamjoh.ui@gmail.com>
2022-11-10 17:54:43 -0500

    Add interaction test

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,4 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 import { Header } from './Header';
 
@@ -10,3 +12,15 @@ export default {
 export const Default: ComponentStory<typeof Header> = (): JSX.Element => (
   <Header />
 );
+
+Default.play = async ({ canvasElement }) => {
+  window.innerWidth = 320;
+  const canvas = within(canvasElement);
+  const el = canvas.getByLabelText('Open Menu') as HTMLButtonElement;
+  await userEvent.click(el);
+  const nav = canvas.getByRole('navigation') as HTMLElement;
+  await waitFor(() => expect(nav).toHaveStyle({ right: '0px' }));
+  const closeBtn = canvas.getByLabelText('Close Menu') as HTMLButtonElement;
+  await userEvent.click(closeBtn);
+  await waitFor(() => expect(nav).toHaveStyle({ right: '-100%' }));
+};
